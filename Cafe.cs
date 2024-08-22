ff64a1b462fabc150d3a51d736793eb6bcda72d0
Martina Kay Basquez <martinabasquez@gmail.com>
2024-08-22 14:36:07 -0500

    added list to main program and updated cafe class

--- Cafe.cs
+++ Cafe.cs
@@ -1,17 +1,21 @@
-namespace POS_Martina;
+namespace PointOfSale;
 
 public class Cafe
 {
    // properties 
-   public string DrinkName { get; set; }
+   public string MenuItem { get; set; }
+   public string Category { get; set; }
+   public string Description { get; set; }
    public decimal Price { get; set; }
 
    public static List<Cafe> menu = new List<Cafe>();
    
    // constructor 
-   public Cafe(string _drinkName, decimal _price)
+   public Cafe(string _menuItem, string _category, string _description, decimal _price)
    {
-      DrinkName = _drinkName;
+      MenuItem = _menuItem;
+      Category = _category;
+      Description = _description;
       Price = _price;
    }
    
--- Program.cs
+++ Program.cs
@@ -4,50 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        static void getPaymentType()
+        Cafe.menu.Add(new Cafe("Iced latte", "drink", "coffee", 7.99M));
+        Cafe.menu.Add(new Cafe("Black coffee", "drink", "coffee", 2.99M));
+        Cafe.menu.Add(new Cafe("Water", "drink", "water", 3.99M));
+        Cafe.menu.Add(new Cafe("Danish", "food", "bread product", 6.99M));
+        
+        Console.WriteLine("Welcome to the Three Musketeers' Coffee Shop:");
+        foreach (var m in Cafe.menu)
         {
-            string paymentType;
-            while (true)
+            Console.WriteLine($"{m.MenuItem} {m.Price}");
+        }
+        Console.WriteLine("What can I get for you today?");
+        
+        Console.WriteLine("How many do you want?");
+        Validator.GetPositiveInputInt();
+
+    }
+    
+    static void getPaymentType()
+    { 
+        string paymentType;
+        while (true)
+        {
+            Console.WriteLine("Please select payment type:");
+            Console.WriteLine("Enter 'cash',  'credit', or 'check'.");
+            paymentType = Console.ReadLine().ToLower();
+            if (paymentType == "cash")
             {
-                Console.WriteLine("Please select payment type:");
-                Console.WriteLine("Enter 'cash',  'credit', or 'check'.");
-                paymentType = Console.ReadLine().ToLower();
-                if (paymentType == "cash")
-                {
-                    payCash();
-                    break;
-                }
-                else if (paymentType == "credit")
-                {
+                payCash();
+                break;
+            }
+            else if (paymentType == "credit")
+            {
 
-                }
-                else if (paymentType == "check")
-                {
+            }
+            else if (paymentType == "check")
+            {
 
-                }
             }
         }
+    }
 
-        static void payCash()
+    static void payCash() 
+    {
+        double amountTendered;
+        while (true)
         {
-            double amountTendered;
-            while (true)
+            Console.WriteLine($"Your total is {totalCost}.");
+            Console.WriteLine("How much cash will you pay with?");
+            try
             {
-                Console.WriteLine($"Your total is {totalCost}.");
-                Console.WriteLine("How much cash will you pay with?");
-                try
-                {
-                    amountTendered = double.Parse(Console.ReadLine());
-                    break;
-                }
-                catch
-                {
-                    Console.WriteLine("Please enter a valid number.");
-                }
+                amountTendered = double.Parse(Console.ReadLine());
+                break;
             }
-            double change = amountTendered - totalCost;
-            Console.WriteLine($"Your change is {change}.");
+            catch
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
         }
-
+        double change = amountTendered - totalCost;
+        Console.WriteLine($"Your change is {change}.");
     }
 }
