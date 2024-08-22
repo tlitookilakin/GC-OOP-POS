ea87e720e6559a89127328397d018130dc5b49c2
Wren <grahamgrasley@gmail.com>
2024-08-22 14:10:12 -0400

    SalesCalculator
    Initial implementation

---
+++ SalesCalculator.cs
@@ -0,0 +1,27 @@
+﻿namespace PointOfSale
+{
+	internal class SalesCalculator
+	{
+		private readonly IEnumerable<object> Purchases;
+
+		public SalesCalculator(IEnumerable<object> purchases)
+		{
+			Purchases = purchases;
+		}
+
+		public decimal GetSubTotal()
+		{
+			return 0m;
+		}
+
+		public decimal GetSalesTax()
+		{
+			return 0m;
+		}
+
+		public decimal GetTotal()
+		{
+			return GetSubTotal() + GetSalesTax();
+		}
+	}
+}
